d67cf7cfd28b6673a5f39f06f96a570a0db27f53
bikesh32 <bikeshsingh023@gmail.com>
2023-08-16 10:28:28 +0530

    first commit

---
+++ init_setup.sh
@@ -0,0 +1,9 @@
+echo [$(date)]: "START"
+echo [$(date)]: "Creating conda env with python version 3.8"
+conda create  -p venv python=3.8 -y
+echo [$(date)]: "activate env"
+source activate ./venv
+echo [$(date)]: "installing dev requirements"
+pip install -r requirements_dev.txt
+echo [$(date)]: "END"
+ 
